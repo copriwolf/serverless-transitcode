4042536b1656506a07b8ef76b78b28b5a62c5953
homandeng <homandeng@wesure.cn>
2020-07-15 13:30:36 +0800

    commit 格式修正

--- src/main.go
+++ src/main.go
@@ -32,16 +32,16 @@ func Decode(content string) (resp RespContent) {
 }
 
 type RequestContent struct {
-	Opt int `json:"opt"`
-	Content string `json:"content"`
+	Opt 		int `json:"opt"`
+	Content 	string `json:"content"`
 }
 
 type RespContent struct {
-	Origin string `json:"origin"`
-	Base64 string `json:"base64"`
-	Html       string `json:"html"`
-	Unicode      string `json:"unicode"`
-	Url  string `json:"url"`
+	Origin     	string `json:"origin"`
+	Base64     	string `json:"base64"`
+	Html       	string `json:"html"`
+	Unicode    	string `json:"unicode"`
+	Url  		string `json:"url"`
 	Utf8ToGbk   string `json:"utf8ToGbk"`
 }
 
